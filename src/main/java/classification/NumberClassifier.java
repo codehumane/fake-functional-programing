309a409e921adcd59301512b532e0b24e9d7abc5
codehumane <codehumane@gmail.com>
2016-11-06 18:04:01 +0900

    NumberClassifier 함수형으로 전환, 완전수/과잉수/부족수 여부 확인 메서드 추가, 설명 추가
    - 메서드들을 static으로 선언하여 상태가 생길 가능성을 제거함
    - 완전수/과잉수/부족수 판단 메서드 추가
    - 고민했던 내용을 추후 다시 살펴볼 수 있도록 코멘트

--- src/main/java/classification/NumberClassifier.java
+++ src/main/java/classification/NumberClassifier.java
@@ -5,18 +5,79 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /**
- * number factor classifier
+ * 자연수 분류 (number factor classifier)
+ *
+ * 닐 포드의 <함수형 사고>라는 책에 따르면 클래스를 좀 더 함수적으로 만들기 위해 모든 메서드를 정적 메서드로 만들라고 한다.<br/>
+ * 이렇게 하면 상태를 가지지 않기 때문에, 상태를 가짐으로써 생기는 모든 문제(같은 코드이고 같은 입력 값을 가지는데 다른 값이 나온다)를 피할 수 있다.<br/>
+ * 더불어 불필요할 수 있는 인스턴스 생성도 피할수 있다.<br/>
+ * 물론 단점도 존재한다. 인자들의 수가 점점 늘어난다. 현재 클래스는 상관 없겠지만 말이다.<br/>
+ * 조금 더 나아갈 수 있지만 이 클래스에서 고민할 내용은 아니므로 생략한다.
  */
 public class NumberClassifier {
 
-    public boolean isFactor(int number, int potential) {
+    /**
+     * potential이 number의 약수인지 여부 판단
+     * @param number 자연수
+     * @param potential 약수
+     * @return
+     */
+    public static boolean isFactor(int number, int potential) {
+        // static으로 선언함에 따라 부수효과가 없는 순수함수가 되었다.
         return number % potential == 0;
     }
 
-    public List<Integer> factorsOf(int number) {
-        return IntStream.rangeClosed(1, number)
-                .filter(x -> isFactor(number, x))
+    /**
+     * number의 모든 약수 반환
+     * @param number 자연수
+     * @return
+     */
+    public static List<Integer> factorsOf(int number) {
+        return streamFactorsOf(number)
                 .boxed()
                 .collect(Collectors.toList());
+    }
+
+    private static IntStream streamFactorsOf(int number) {
+        // 아직 아무것도 실행되지 않았다. 게으른 평가이다.
+        return IntStream
+                .rangeClosed(1, number)
+                .filter(x -> isFactor(number, x));
+    }
+
+    /**
+     * number의 모든 약수 합
+     * @param number 자연수
+     * @return
+     */
+    public static int sumFactors(int number) {
+        return streamFactorsOf(number).sum();
+    }
+
+    /**
+     * 완전수(자기 자신을 제외한 약수들의 합이 자기 자신과 같음) 여부 판단
+     * @param number
+     * @return
+     */
+    public static boolean isPerfect(int number) {
+        // '자기 자신을 제외한'을 좀더 명시적으로 드러내기 위해 - 연산자를 사용함
+        return sumFactors(number) - number == number;
+    }
+
+    /**
+     * 과잉수 여부 판단
+     * @param number
+     * @return
+     */
+    public static boolean isAbundant(int number) {
+        return sumFactors(number) - number > number;
+    }
+
+    /**
+     * 부족수 여부 판단
+     * @param number
+     * @return
+     */
+    public static boolean isDeficient(int number) {
+        return sumFactors(number) - number < number;
     }
 }
--- src/test/java/classification/NumberClassifierTest.java
+++ src/test/java/classification/NumberClassifierTest.java
@@ -1,49 +1,41 @@
 package classification;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class NumberClassifierTest {
-
-    private NumberClassifier classifier;
-
-    @Before
-    public void setUp() {
-        this.classifier = new NumberClassifier();
-    }
 
     @Test
     public void isFactor_1은_1의_인수이다() throws Exception {
-        Assert.assertTrue(classifier.isFactor(1, 1));
+        Assert.assertTrue(NumberClassifier.isFactor(1, 1));
     }
 
     @Test
     public void isFactor_2는_4의_인수이다() throws Exception {
-        Assert.assertTrue(classifier.isFactor(4, 2));
+        Assert.assertTrue(NumberClassifier.isFactor(4, 2));
     }
 
     @Test
     public void isFactor_3는_4의_인수가_아니다() throws Exception {
-        Assert.assertFalse(classifier.isFactor(4, 3));
+        Assert.assertFalse(NumberClassifier.isFactor(4, 3));
     }
 
     @Test
     public void isFactor_5는_4의_인수가_아니다() throws Exception {
-        Assert.assertFalse(classifier.isFactor(5, 4));
+        Assert.assertFalse(NumberClassifier.isFactor(5, 4));
     }
 
     @Test
     public void factorsOf_1의_인수목록은_1이다() throws Exception {
         // given
-        List<Integer> expected = new ArrayList<Integer>();
+        List<Integer> expected = new ArrayList<>();
         expected.add(1);
 
         // when
-        List<Integer> result = classifier.factorsOf(1);
+        List<Integer> result = NumberClassifier.factorsOf(1);
 
         // then
         Assert.assertEquals(expected, result);
@@ -52,14 +44,14 @@ public class NumberClassifierTest {
     @Test
     public void factorsOf_6의_인수목록은_1236이다() throws Exception {
         // given
-        List<Integer> expected = new ArrayList<Integer>();
+        List<Integer> expected = new ArrayList<>();
         expected.add(1);
         expected.add(2);
         expected.add(3);
         expected.add(6);
 
         // when
-        List<Integer> result = classifier.factorsOf(6);
+        List<Integer> result = NumberClassifier.factorsOf(6);
 
         // then
         Assert.assertEquals(expected, result);
@@ -68,14 +60,40 @@ public class NumberClassifierTest {
     @Test
     public void factorsOf_13의_인수목록은_1과_13이다() throws Exception {
         // given
-        List<Integer> expected = new ArrayList<Integer>();
+        List<Integer> expected = new ArrayList<>();
         expected.add(1);
         expected.add(13);
 
         // when
-        List<Integer> result = classifier.factorsOf(13);
+        List<Integer> result = NumberClassifier.factorsOf(13);
 
         // then
         Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void sumFactors_6의_인수_총합은_12이다() throws Exception {
+        Assert.assertEquals(12, NumberClassifier.sumFactors(6));
+    }
+
+    @Test
+    public void isPerfect_6은_완전수이다() throws Exception {
+        Assert.assertTrue(NumberClassifier.isPerfect(6));
+        Assert.assertFalse(NumberClassifier.isAbundant(6));
+        Assert.assertFalse(NumberClassifier.isDeficient(6));
+    }
+
+    @Test
+    public void isAbundant_12는_과잉수이다() throws Exception {
+        Assert.assertFalse(NumberClassifier.isPerfect(12));
+        Assert.assertTrue(NumberClassifier.isAbundant(12));
+        Assert.assertFalse(NumberClassifier.isDeficient(12));
+    }
+
+    @Test
+    public void isDeficient_8은_부족수이다() throws Exception {
+        Assert.assertFalse(NumberClassifier.isPerfect(8));
+        Assert.assertFalse(NumberClassifier.isAbundant(8));
+        Assert.assertTrue(NumberClassifier.isDeficient(8));
     }
 }
